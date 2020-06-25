43be041a37051ed21311d02b92d31a9d406828d0
migueldf10 <migueldf10@gmail.com>
2020-06-25 10:52:28 +0200

    Error in not async routes

--- routers/image.js
+++ routers/image.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Image = require('../models').image
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
 	try {
 		const allImages = await Image.getAll()
 		console.log(allImages)
--- routers/user.js
+++ routers/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models').user
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
 	try {
 		const allUsers = await User.findAll()
 		console.log(allUsers)
